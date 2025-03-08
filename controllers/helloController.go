97bb57ac157acf77afbc5f6f475ed13af33511ab
Mauricio Rojas <mrojasb2000@gmail.com>
2025-03-08 14:16:22 -0300

    refactor: add path '/' pattern to act like a catch-all validation

--- controllers/helloController.go
+++ controllers/helloController.go
@@ -5,5 +5,11 @@ import "net/http"
 // Define a home function which writes a byte slice containing
 // "Hello from Snippetbox" as the response body.
 func Home(w http.ResponseWriter, r *http.Request) {
+	// Check if the current request URL path exactly matches "/". If it doesn't,
+	// use the http.NotFound() function to send a 404 response to the client.
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
 	w.Write([]byte("Hello from Snippetbox"))
 }
